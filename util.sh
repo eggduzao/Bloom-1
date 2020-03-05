0555b68d68a854eb5ed07c1d3b082bfa1cc8a282
eggduzao <eggduzao@gmail.com>
2020-03-05 18:55:27 +0100

    a

--- util.sh
+++ util.sh
@@ -3,4 +3,6 @@ find . -name "*.DS_Store*" -type f -delete
 find . -name "*.Rout" -type f -delete
 find . -name "*.RData" -type f -delete
 find . -name "*.pyc" -type f -delete
+git config --global user.name "eggduzao"
+git config --global user.email eggduzao@gmail.com
 git config --global credential.helper 'cache --timeout=3600000000000'
