f4f84438b564294ae5134c7afd4a86def694b342
eggduzao <eggduzao@gmail.com>
2020-12-10 20:16:43 +0100

    a

---
+++ bloom/preprocess.py
@@ -0,0 +1,64 @@
+"""
+Preprocess Module
+===================
+Placeholder.
+
+Authors: Eduardo G. Gusmao.
+
+"""
+
+###################################################################################################
+# Libraries
+###################################################################################################
+
+# Python
+
+# Internal
+
+# External
+
+###################################################################################################
+# Basic Objects
+###################################################################################################
+
+class Preprocess():
+  """This class represents TODO.
+
+  *Keyword arguments:*
+
+    - argument1 -- Short description. This argument represents a long description. It can be:
+      - Possibility 1: A possibility 1.
+      - Possibility 2: A possibility 2.
+
+    - argument2 -- Short description. This argument represents a long description. It can be:
+      - Possibility 1: A possibility 1.
+      - Possibility 2: A possibility 2.
+    """
+
+  def __init__(self):
+    """Returns TODO.
+    
+    *Keyword arguments:*
+    
+      - argument -- An argument.
+    
+    *Return:*
+    
+      - return -- A return.
+    """
+    pass
+
+  def randadd(self):
+
+    # Randomly add imputed signal. Add more random fragments arounnd intense signals
+
+    # Randadd shape = circle?
+
+    # Clean matrix of very low signals
+
+  def posttad(self):
+
+    # Re-create insulation at the TAD regions
+
+
+
