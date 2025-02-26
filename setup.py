e9168de494d848fc4a1e2c3da37e3f753fc72e7a
eggduzao <eggduzao@gmail.com>
2025-02-26 15:14:46 -0300

    recreating structure

--- setup.py
+++ setup.py
@@ -1,235 +1,178 @@
-from __future__ import print_function
-"""
-Setup Module
-===================
-Placeholder.
 
-Authors: Eduardo G. Gusmao. 
+# Stainalyzer Package
 
-Install Command: pip3.9 install --user .
-                 pip3.9 install --user . --upgrade
-"""
-
-# Python
 import os
-import io
 import re
-import sys
-import pwd
-import shutil
-import optparse
-import setuptools
+from setuptools import setup, find_packages  # setuptools is the standard tool for packaging Python projects.
 
+def read_version():
+    version_file = os.path.join(os.path.dirname(__file__), 'Stainalyzer', '__init__.py')
+    with open(version_file, 'r') as f:
+        version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", f.read(), re.M)
+        if version_match:
+            return version_match.group(1)
+        raise RuntimeError("Unable to find version string.")
 
-###################################################################################################
-# TODO Big Unsolved TODOs TODO
-###################################################################################################
+# setup() is the function that handles all the package metadata and configuration.
+setup(
+    # Name of your package. This should be unique and is how it will be identified on PyPI.
+    name='Stainalyzer',  
 
-# Make chromosomes general based on the name provided by the user.
-# Optimization at cpu level.
-# Optimization at gpu level.
+    # Version of your package. Follows semantic versioning (e.g., MAJOR.MINOR.PATCH).
+    version=read_version(),  
 
-###################################################################################################
-# Unsupported Platforms
-###################################################################################################
+    # A brief, one-line description of what your package does.
+    description='A self-supervised learning toolkit for histopathology microscopy image analysis.',  
 
-supported_platforms = ["linux", "linux2", "darwin"]
-if sys.platform not in supported_platforms:
-    print("ERROR: This package currently supports only unix-based systems (Linux and MAC OS X).")
-    sys.exit(0)
+    # A longer description of your project. Often pulled from your README file.
+    long_description=open('README.md').read(),  
+    
+    # Specify the format of your long description. Common options: 'text/markdown' or 'text/x-rst'.
+    long_description_content_type='text/markdown',  
 
+    # Author's name.
+    author='Eduardo Gade Gusmao',  
 
-###################################################################################################
-# Parameters
-###################################################################################################
+    # Author's email.
+    author_email='eduardo.gusmao@tum-sls.de',  
 
-"""
-Tools Dictionary:
-  * Insert in the dictionary bellow a key+tuple X: (Y,Z,W,K) representing:
-    - X: A string representing the name the user should provide for this script in order to install the tool.
-    - Y: A string representing the name of the program which the user should type in the terminal in order to execute the tool.
-    - Z: A string representing the path to the main function that executes the program.
-    - W: A list with the package requirements for that program.
-    - K: A list with external binary files that will be copied by the setup installation function to the user's bin folder.
-Tools Dictionary Standard:
-  * All programs should start with "bk-" followed by the program name.
-  * The main function called within the script must be termed "main".
-"""
+    # URL for the project, e.g., GitHub repository.
+    url='https://github.com/eggduzao/Stainalyzer',  
 
-# Common dependencies.
-common_deps = ["numpy>=1.12.0",
-               "scipy>=1.4.0"]
+    # List of keywords to help people find your package when searching PyPI.
+    keywords='DAB, immunohistopathology, self-supervised learning, image analysis, deep learning',  
 
-tools_dictionary = {
-  "bloom": (
-    "bloom",
-    "bloom.main:main",
-    [],
-    []
-  )
-}
+    # Automatically find all packages in the current directory, excluding certain patterns.
+    packages=find_packages(exclude=['dev', 'test', 'tests', 'docs', 'data', 'examples']),  
 
+    # Include additional files specified in MANIFEST.in.
+    include_package_data=True,  
 
-###################################################################################################
-# Auxiliary Functions/Classes
-###################################################################################################
+	# List of dependencies that will be installed when someone installs your package.
+	install_requires=[
+	    'numpy',       					# For numerical computations, compatible with numpy 2.0.2
+	    'scipy',       					# Scientific computing library, compatible with scipy 1.13.1
+	    'torch',        				# PyTorch for deep learning, compatible with torch 2.6.0
+	    'torchvision', 					# Image transformations and datasets for PyTorch, compatible with torchvision 0.21.0
+	    'pandas',       				# Data manipulation and analysis, compatible with pandas 2.2.3
+	    'opencv-python', 				# Image processing, compatible with OpenCV 4.10.0
+	    'openpyxl', 				    # Reading and writing Excel files, compatible with openpyxl 3.1.5
+	    'seaborn',    				    # Data visualization, compatible with seaborn 0.13.2
+	    'Pillow',      				    # Python Imaging Library, compatible with Pillow 10.4.0
+	    'matplotlib',  				    # Plotting and visualization, compatible with matplotlib 3.9.2
+	    'scikit-learn',				    # Machine learning utilities, compatible with scikit-learn 1.5.2
+	    'scikit-image',					# Machine learning image utilities, compatible with scikit-image 0.25.0
+	    'colour', 						# Color science utilities, compatible with colour 0.4.6
+	    'sympy'							# Symbolic mathematics library, compatible with sympy 4.1.1
+	],						
 
-# PassThroughOptionParser Class
-class PassThroughOptionParser(optparse.OptionParser):
-  """
-  An 'unknown option' pass-through implementation of OptionParser.
-  When unknown arguments are encountered, bundle with largs and try again,
-  until rargs is depleted.
-  sys.exit(status) will still be called if a known argument is passed
-  incorrectly (e.g. missing arguments or bad argument types, etc.)
-  """
+    # Additional groups of dependencies, e.g., for development or testing.
+	extras_require={
 
-  def _process_args(self, largs, rargs, values):
-    while rargs:
-      try:
-        optparse.OptionParser._process_args(self, largs, rargs, values)
-      except (optparse.BadOptionError, optparse.AmbiguousOptionError) as err:
-        largs.append(err.opt_str)
+	    # Development tools for maintaining code quality, formatting, and versioning
+	    'dev': [
+	        'black',             # Automatic code formatter for consistent style
+	        'flake8',            # Linter for identifying code issues and enforcing PEP8
+	        'isort',             # Automatically sort and organize imports
+	        'pre-commit',        # Manage and run pre-commit hooks for code checks
+	        'bump2version',      # Automate version bumping for releases
+	        'mypy',              # Optional static type checker for Python
+	        'tox',               # Test automation across multiple environments (can overlap with testing)
+	        'pylint'             # Code analysis tool for identifying potential issues
+	    ],
 
-# recursive_chown_chmod Function
-def recursive_chown_chmod(path_to_walk, uid, gid, file_permission, path_permission):
-  """
-  Recursively applies chown from path.
-  """
-  for root_dir, directory_list, file_list in os.walk(path_to_walk):
-    os.chown(root_dir, uid, gid)
-    os.chmod(root_dir, path_permission)
-    for f in file_list:
-      current_complete_file = os.path.join(root_dir, f)
-      os.chown(current_complete_file, uid, gid)
-      os.chmod(current_complete_file, file_permission)
+	    # Testing tools for running, managing, and measuring test coverage
+	    'test': [
+	        'pytest',            # Framework for writing and running unit tests
+	        'pytest-cov',        # Plugin for measuring code coverage with pytest
+	        'coverage',          # Track which parts of the code are covered by tests
+	        'hypothesis',        # Property-based testing, generates edge cases automatically
+	        'tox',               # Automate testing across multiple environments (also useful in dev)
+	        'faker',             # Generate fake data for testing
+	        'responses'          # Mock out HTTP responses in tests
+	    ],
 
-def read(*names, **kwargs):
-  with io.open(
-    os.path.join(os.path.dirname(__file__), *names),
-    encoding=kwargs.get("encoding", "utf8")
-  ) as fp:
-    return fp.read()
+	    # Documentation tools for generating and managing project documentation
+	    'docs': [
+	        'sphinx',            		# Powerful documentation generator
+	        'sphinx-rtd-theme',  		# ReadTheDocs theme for Sphinx
+	        'sphinx-autodoc-typehints', # Automatically include type hints in docs
+	        'myst-parser',       		# Support for Markdown in Sphinx
+	        'nbsphinx',          		# Integrate Jupyter Notebooks into documentation
+	        'mkdocs',            		# Alternative static site generator for docs
+	        'mkdocs-material'    		# Popular theme for MkDocs
+	    ]
+	},
 
-def find_version(*file_paths):
-  version_file = read(*file_paths)
-  version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
-  if version_match:
-    return version_match.group(1)
-  raise RuntimeError("Unable to find version string.")
+	# Fine-grained control of what to include in addition
+	package_data={
+	    'Stainalyzer': [
+	        'data/colornames.txt',          # Example datasets in CSV format
+	        'data/DAB_Training',			# Sample training images
+	        'data/DAB_Training_Output',     # Sample training images
+	        'datapath.py',					# Script to clean the DAB-Project data tree
+	        'clean.sh',           			# Script to clean MAC OS X trash files
+	        'rsync.sh'           			# Script to convert data between cluster and local
+	    ]
+	},
 
+    # Specifies the license under which your package is distributed.
+    license='MIT',  
 
-###################################################################################################
-# Processing Input Arguments
-###################################################################################################
+    # Classifiers provide metadata about your package for PyPI and users.
+    classifiers=[
 
-# Parameters
-current_version = find_version("bloom", "__version__.py")
-usage_message = "python setup.py install [python options] [Bloom options]"
-version_message = "Bloom. Version: " + str(current_version)
+    	# Official Stage
+        'Development Status :: 3 - Alpha',  # Indicates maturity level of the project
 
-# Initializing Option Parser
-parser = PassThroughOptionParser(usage = usage_message, version = version_message)
+        # Target audience
+        'Intended Audience :: Science/Research',
 
-# Parameter: Copy bloom data folder
-param_copy_bloom_data_name = "--copy-bloom-data"
-param_copy_bloom_data_help = "Explain here."
-parser.add_option(param_copy_bloom_data_name, dest="param_copy_bloom_data", action="store_true", default=False, help=param_copy_bloom_data_help)
+	    # Topics
+	    'Topic :: Scientific/Engineering :: Bio-Informatics',
+	    'Topic :: Scientific/Engineering :: Image Recognition',
+	    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+	    'Topic :: Scientific/Engineering :: Artificial Intelligence :: Deep Learning',
+	    'Topic :: Scientific/Engineering :: Mathematics',
+	    'Topic :: Scientific/Engineering :: Mathematics :: Probability and Statistics',
+	    'Topic :: Scientific/Engineering :: Medical Science Apps.',
+	    'Topic :: Scientific/Engineering :: Visualization',
 
-# Processing Options
-options, arguments = parser.parse_args()
-param_copy_bloom_data = options.param_copy_bloom_data
+	    # License Type
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 
-# Manually Removing Additional Options from sys.argv
-new_sys_argv = []
-for e in sys.argv:
-  if param_copy_bloom_data_name == e[:len(param_copy_bloom_data_name)]:
-    continue
-  new_sys_argv.append(e)
-sys.argv = new_sys_argv
+        # Supported Python versions
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12'
+    ],
 
-# Defining entry points
-current_entry_points = {"console_scripts": []}
-for tool_option in tools_dictionary.keys():
-  current_entry_points["console_scripts"].append(" = ".join(tools_dictionary[tool_option][:2]))
+	# Official Stages:
+	# Status					Code	Description
+	# 1 - Planning				1		Initial planning, no functional code yet.
+	# 2 - Pre-Alpha				2		Early development, incomplete and unstable.
+	# 3 - Alpha					3		Limited functionality, experimental, may be unstable.
+	# 4 - Beta					4		Feature-complete but may still have bugs, open for testing.
+	# 5 - Production/Stable		5		Stable release, suitable for general use.
+	# 6 - Mature				6		Fully mature, unlikely to change significantly.
+	# 7 - Inactive				7		No longer maintained or supported.
 
-# Defining install requirements
-current_install_requires = common_deps
-for tool_option in tools_dictionary.keys():
-  current_install_requires += tools_dictionary[tool_option][2]
+    # This specifies the minimum Python version required.
+    python_requires='>=3.7',  
 
+    # Entry points define executable scripts that can be run from the command line.
+    entry_points={
+        'console_scripts': [
+            'Stainalyzer=Stainalyzer.main:main'  # Creates a command 'Stainalyzer' to run 'main()'' in 'Stainalyzer/Stainalyzer/main.py'
+        ]
+    },
 
-###################################################################################################
-# Creating Data Path
-###################################################################################################
-
-# Default bloom_data_path
-current_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "bloom_data")
-bloom_data_path = os.path.join(os.path.expanduser("~"), "bloom_data")
-
-# Creating data path # TODO
-if(param_copy_bloom_data):
-  if(os.path.exists(bloom_data_path)):
-    shutil.rmtree(bloom_data_path)
-  shutil.copytree(current_path, bloom_data_path)
-
-
-###################################################################################################
-# Setup Function
-###################################################################################################
-
-# Parameters
-package_name = "Bloom"
-package_version = str(current_version)
-short_description = "Computational Framework to reveal occult patterns in 3C contact matrices"
-classifiers_list = ["Topic :: Scientific/Engineering :: Bio-Informatics",
-                    "Topic :: Scientific/Engineering :: Artificial Intelligence"]
-keywords_list = ["NGS", "Hi-C", "DPMM"]
-author_list = ["Eduardo G. Gusmao"]
-corresponding_mail = "eduardo.gusmao@med.uni-goettingen.de"
-license_type = "GPL"
-
-# Fetching additional structural files
-readme_file_name = os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.md")
-
-# Fetching long description
-readme_file = open(readme_file_name, "rU", encoding="utf-8")
-long_description = readme_file.read()
-readme_file.close()
-
-# Setup Function
-setuptools.setup(name = package_name,
-                 version = package_version,
-                 description = short_description,
-                 long_description = long_description,
-                 classifiers = classifiers_list,
-                 keywords = ", ".join(keywords_list),
-                 author = ", ".join(author_list),
-                 author_email = corresponding_mail,
-                 license = license_type,
-                 packages = setuptools.find_packages(),
-                 entry_points = current_entry_points,
-                 install_requires = current_install_requires
+    # Project-related URLs.
+    project_urls={
+        'Source': 'https://github.com/yourusername/microscopy'
+    },
 )
-
-
-###################################################################################################
-# Termination
-###################################################################################################
-
-# Modifying Permissions when Running Superuser/Admin
-if(param_copy_bloom_data):
-
-  # Get current user and set default permissions for files to be visible and binaries executable
-  current_user = os.getenv("SUDO_USER")
-  default_file_permission = 0o644
-  default_path_permission = 0o755
-
-  # Setting the permissions
-  if current_user:
-    current_user_uid = pwd.getpwnam(current_user).pw_uid
-    current_user_gid = pwd.getpwnam(current_user).pw_gid
-    recursive_chown_chmod(bloom_data_path, current_user_uid, current_user_gid, default_file_permission,
-                          default_path_permission)
-
-
